dc2e79259973455101f66c45bf34abe20288adaf
Bibrata Barman <33283305+iambibro@users.noreply.github.com>
2019-10-18 15:38:08 +0530

    update

--- 13_palindrome.asm
+++ 13_palindrome.asm
@@ -1,42 +1,52 @@
-;13_//palindrome program
+; This program checks if a string is a palindrome or not
+;plindrome is text that can be read backwords and give the same meaning as if it was read forward
 
-    name "pali"
-    org 100h
-    jmp start
-msg1: db "This is Palindrome $"
-msg2: db "This is not Palindrome $"
-;m1 : s db "abb"
-m1  : s db "able was ere ere saw elba"
-s_size=$-m1
-db 00h,0ah,'$'
+        
+        
+        name "pali"
+        org 100h
+        jmp start     
+        
+        msg1 db "!!! This is palindrome !!!$"
+        msg2 db "!!!! This is not a palindrome !!!!$"
+        
+m1:     s db 'able was ere ere saaw elba'  
 
-start: mov ah,09h
-       mov dx,offset s
-       int 21h        
-       lea di,s
-       add si,di
-       add si,s_size  
-       dec si
-       mov cx,s_size
-       cmp cx,01h
-       jc is_palindrome
-       shr cx,01h
-next_char:mov al,[di]
-          mov bl,[si]
-          cmp al,bl
-          jnc not_palindrome
-          inc di
-          dec si
-          loop next_char
-is_palindrome:mov ah,09h
-              mov dx,offset msg1
-              int 21h
-              jmp stop
-not_palindrome:mov ah,09h
-               mov dx,offset msg2
-               int 21h
-       stop:   mov ah,0h
-               int 16h
-               ret
-       
-                                   
+        s_size=$-m1 
+        
+        db 0dh, 0ah, '$'
+        
+start:  mov ah,09h
+        mov dx, offset s
+        int 21h
+        ;Display the Input
+        
+        lea di,s
+        mov si,di
+        add si,s_size
+        dec si
+        mov cx,s_size
+        cmp cx,01h
+        je is_palindrome
+        shr cx,01h
+        
+next_char:  mov al,[di]
+            mov bl,[si]
+            cmp al ,bl
+            jne not_palindrome
+            inc di
+            dec si
+            loop next_char
+            
+is_palindrome:  mov ah,09h
+                mov dx,offset msg1
+                int 21h
+                jmp stop
+                
+not_palindrome: mov ah,09h
+                mov dx,offset msg2
+                int 21h
+                
+stop:           mov ah,00h
+                int 16h
+                ret 
